efa04e62746154c6416877a6c8bb7911c51cd3af
Chheng Kimter <Chhengkimter@gmail.com>
2025-12-30 19:07:31 +0700

    add main class

--- src/ssession.java
+++ src/ssession.java
@@ -27,4 +27,33 @@ public class ssession {
         this.startTime = startTime;
         this.endTime = endTime;
     }
+
+    public static void main(String[] args) {
+     
+        ssession session1 = new ssession(900, 1100, "Conference Room A");
+        System.out.println("Session 1:");
+        System.out.println("  Start Time: " + session1.getStartTime());
+        System.out.println("  End Time: " + session1.getEndTime());
+        System.out.println("  Location: " + session1.getLocation());
+        System.out.println();
+
+        ssession session2 = new ssession(1300, 1500, "Auditorium");
+        System.out.println("Session 2:");
+        System.out.println("  Start Time: " + session2.getStartTime());
+        System.out.println("  End Time: " + session2.getEndTime());
+        System.out.println("  Location: " + session2.getLocation());
+        System.out.println();
+
+        session1.setDuration(900, 1200);
+        System.out.println("Session 1 Updated:");
+        System.out.println("  Start Time: " + session1.getStartTime());
+        System.out.println("  End Time: " + session1.getEndTime());
+        System.out.println();
+
+        try {
+            ssession invalidSession = new ssession(1500, 1500, "Room B");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error creating invalid session: " + e.getMessage());
+        }
+    }
 }
